8044c5ee4dab7b910c0f2108a117b6ed608e9bd5
Wellington <wellingtonwar17@gmail.com>
2022-11-11 20:06:32 -0300

    first commit

---
+++ backend/src/main/java/com/pw1_2021/FourInfo/FourInfoApplication.java
@@ -0,0 +1,13 @@
+package com.pw1_2021.FourInfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FourInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FourInfoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/pw1_2021/FourInfo/FourInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pw1_2021.FourInfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FourInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
